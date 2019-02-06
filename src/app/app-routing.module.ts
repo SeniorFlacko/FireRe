0110ef1dafd56ded4d6035f1921331bd89401228
Cristian <calemanfuentes@amarello.com.mx>
2019-02-06 14:35:38 -0600

    Separar Dashboard y las Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login', 
+    component: LoginComponent
+  },
+  {
+    path:'registro', 
+    component: RegisterComponent
+  },
+  {
+    path:'dashboard', 
+    component: DashboardComponent
+  },
+  {
+    path:'', 
+    component: DashboardComponent
+  },
+  { // Para cualquier otro valor redirigir al dashboard
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
