9c574f9f6e58ee07282cf1854b6d86d2d5df79c0
Cristian Aleman <cristianaleman@MacBook-Air-de-Cristian.local>
2019-02-10 14:07:40 -0600

    Usar propiedad isloading del ui state para poner un loader

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IngresoEgreso } from '../models/ingreso-egreso.model';
 import { IngresoEgresoService } from './ingreso-egreso.service';
 import swal from 'sweetalert';
+import { AppState } from '../app.reducer';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { ActivarLoaderAction, DesactivarLoaderAction } from '../shared/ui.actions';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
   styles: []
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
 
   private egresoIngresoForm: FormGroup;
   private tipo = 'ingreso'; 
+  public loading: boolean;
+  private loading$: Subscription =  new Subscription();
 
-  constructor(private ingresoEgresoService: IngresoEgresoService) { }
+  constructor(
+    private ingresoEgresoService: IngresoEgresoService,
+    private store: Store<AppState>
+    ) { 
+      this.loading$ = this.store.select('ui').subscribe(ui =>{
+        this.loading = ui.isLoading;
+      })
+
+    }
 
   ngOnInit() {
     this.egresoIngresoForm =  new FormGroup({
@@ -22,11 +36,16 @@ export class IngresoEgresoComponent implements OnInit {
       'monto': new FormControl('', [Validators.required, Validators.min(1)])
     });
   }
+  ngOnDestroy() {
+    this.loading$.unsubscribe();
+  }
 
   crearIngresoEgreso(){
     let ingresoEgreso: IngresoEgreso = this.egresoIngresoForm.value;
     ingresoEgreso.tipo = this.tipo;
     console.log(this.egresoIngresoForm.value);
+
+    this.store.dispatch(new ActivarLoaderAction());
     this.ingresoEgresoService
       .crearIngresoEgreso(ingresoEgreso)
       .then(() => {
@@ -35,6 +54,10 @@ export class IngresoEgresoComponent implements OnInit {
           monto: 0
         });
       })
+      .catch(err => console.error(err))
+      .finally(() => {
+        this.store.dispatch(new DesactivarLoaderAction());
+      });
     
   }
 
